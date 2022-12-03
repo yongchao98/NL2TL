8be0abdcca02f280227d9f981191b781f988c206
yongchao98 <yongchaochen@fas.harvard.edu>
2022-12-02 20:28:39 -0500

    modify

--- run_trained_model.py
+++ run_trained_model.py
@@ -31,19 +31,31 @@ model.eval()
 # Here you can define the imperative natural sentences to express orders, like the following example
 # Here we represent atomic propositions with ( prop_* )
 
+# You can define the tested natural langauge directly like the following
+'''
 test_sentence = [
     'If ( prop_4 ) happens and implies ( prop_2 ) and this scenario continues to hold until at some point during the 243 to 582 time units ( prop_3 ) is detected , then the scenario is equivalent to ( prop_1 ) .',
     'If it is not the case that ( prop_3 ) is detected for each time instant in the coming 164 to 612 time units , or else ( prop_1 ) happens , then ( prop_2 ) .',
-    'If at some point ( prop_3 ) and ( prop_2 ) , and is equivalent to ( prop_4 ) , and this scenario will hold until at some other point ( prop_1 ) is detected .',
-
-    ]
+    'If at some point ( prop_3 ) and ( prop_2 ) , and is equivalent to ( prop_4 ) , and this scenario will hold until at some other point ( prop_1 ) is detected .',    ]
+'''
 
 '''
-# Another way is to load the excel files, there are some example files in the dataset dir
-file_name = '/output_davinci_gen_ltl_long_8'
-df = pd.read_excel(home_path_nl2stl+'/dataset/'+file_name+'.xlsx')
-test_sentence = [df['paraphrased_logic_sentence'][i] for i in range(len(df)) if df['Mark'][i]==1]
+# Another way is to load the excel files, there are some example files in the dataset dir, also here we use one example file
+file_name = 'example_excel_nl.xlsx'
+df = pd.read_excel(file_name)
+test_sentence = [df['paraphrased_logic_sentence'][i] for i in range(len(df))]
 '''
+
+# Another way is to load the txt file
+my_file = open("example_excel_nl.txt", "r") 
+# reading the file
+data_test_sentence = my_file.read()
+  
+# replacing end splitting the text 
+# when newline ('\n') is seen.
+test_sentence = data_test_sentence.split("\n")
+my_file.close()
+
 
 home_path_output = home_path_nl2stl + '/application_test/'
 if not os.path.exists(home_path_output):
